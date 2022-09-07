b6761e6c8d5e175f1c3b158d50a9d735afa7e3a7
prashant182002 <prashant182002@gmail.com>
2022-09-07 16:45:51 +0530

    saerkjh

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -17,6 +17,30 @@ class MainScreenState extends State<MainScreen>{
               ReCard(textshow: "Chest",imageshow: "assets/pecks.png"),
           ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+      ),
     );
   }
 }
