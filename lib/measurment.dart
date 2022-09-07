14d2ef860054fc3203c418b8218c485b5b21a2c4
pratt <pp56355@gmail.com>
2022-09-07 19:13:36 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/measurment.dart
    #	lib/welcome_screen.dart

--- lib/measurment.dart
+++ lib/measurment.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'card.dart';
+
 class MeasScreen extends StatefulWidget {
   @override
   MeasScreenState createState() => MeasScreenState();
 }
-class MeasScreenState extends State<MeasScreen>{
+
+class MeasScreenState extends State<MeasScreen> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          ReCard(textshow: 'Chest', imageshow: Image)
+          // ReCard(textshow: 'Chest', imageshow: Image)
         ],
       ),
     );
